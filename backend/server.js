b2558636c93e07fa68816560edc45a3190127a10
euxxx28 <euxcin5@gmail.com>
2025-04-19 09:54:12 +0800

    Updated server.js

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const path = require("path");
 
 const app = express();
-const PORT = 3001;
+const PORT = 4000;
 
 const filePath = path.join(__dirname, "compliments.json");
 
